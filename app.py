66fc95a1e155415ee9fac1c6cb8a3546df05e4c8
whitesapphire615 <aajihad26@gmail.com>
2026-04-18 10:34:38 +0700

    new

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, json
+import os
 
 app = Flask(__name__)
 @app.route('/')
@@ -51,3 +52,4 @@ def init_app():
 if __name__ == "__main__":
     init_app()
     app.run()
+    #host="0.0.0.0", port=int(os.environ.get("PORT", 5000))
